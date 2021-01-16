592667dfebfb099ba7490eae881c0542d3d9977d
Dreamwalker <aristojeff@gmail.com>
2021-01-16 14:47:19 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dooly_creator/res/app_string.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'ui/dooly_main_page.dart';
 
@@ -11,19 +11,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: '둘리 짤 생성기',
+      title: APP_TITLE,
       darkTheme: ThemeData.dark(),
       theme: ThemeData.light().copyWith(primaryColor: Colors.red[500]),
       home: MainPage(),
-
     );
   }
 }
-
-
-
