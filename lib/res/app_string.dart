5d19f66464a7bced34ace8cd42d3c5452fa357c2
Dreamwalker <aristojeff@gmail.com>
2021-01-16 14:21:16 +0900

    Create app_string.dart

---
+++ lib/res/app_string.dart
@@ -0,0 +1,15 @@
+/// 'assets/img/welcome.png',
+//     'assets/img/damage.png',
+//     'assets/img/want_bob.png',
+//     'assets/img/becare.png',
+//     'assets/img/line.png',
+//     'assets/img/lol.png',
+//     'assets/img/beam.png',
+//     'assets/img/time_cosmos.png',
+const String IMG_WELCOME = "assets/img/welcome.png";
+const String IMG_DAMAGE = "assets/img/damage.png";
+const String IMG_WANT_BOB = "assets/img/want_bob.png";
+const String IMG_BECARE_WELL = "assets/img/becare.png";
+const String IMG_LINE_OUT = "assets/img/line.png";
+const String IMG_LOL = "assets/img/lol.png";
+
