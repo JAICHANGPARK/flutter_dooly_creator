c67618283be451b63d1f75cc2ea483e9f2c0756e
Dreamwalker <aristojeff@gmail.com>
2021-01-16 14:24:23 +0900

    Update becare_well_page.dart

--- lib/ui/editor/becare_well_page.dart
+++ lib/ui/editor/becare_well_page.dart
@@ -336,40 +336,4 @@ class _BecareWellDoolyEditorPageState extends State<BecareWellDoolyEditorPage> {
     return true;
   }
 
-  void _capture() async {
-    print("START CAPTURE");
-    String datetime = DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.now());
-    var renderObject = globalKey.currentContext.findRenderObject();
-    if (renderObject is RenderRepaintBoundary) {
-      var boundary = renderObject;
-      ui.Image image = await boundary.toImage();
-      final directory = (await getApplicationDocumentsDirectory()).path;
-      print(directory);
-      ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-      Uint8List pngBytes = byteData.buffer.asUint8List();
-      print(pngBytes);
-      File imgFile = new File('$directory/want_dooly_${datetime}.png');
-      try {
-        imgFile.writeAsBytes(pngBytes);
-        print("FINISH CAPTURE ${imgFile.path}");
-      } catch (e) {
-        print(e);
-      }
-
-      try {
-        final result = await ImageGallerySaver.saveImage(pngBytes, quality: 100, name: "want_dooly_$datetime");
-        if(result['isSuccess'] == true){
-          Fluttertoast.showToast(msg: "저장성공");
-        }else{
-          Fluttertoast.showToast(msg: "저장실패");
-        }
-        
-        print(result);
-      } catch (e) {
-        print(e);
-      }
-    } else {
-      print("!");
-    }
-  }
 }
