ded542c0188286e2c4c64e6e5c340606ab44c0da
Dreamwalker <aristojeff@gmail.com>
2021-01-16 14:24:22 +0900

    Update line_out_page.dart

--- lib/ui/editor/line_out_page.dart
+++ lib/ui/editor/line_out_page.dart
@@ -249,7 +249,7 @@ class _LineOutDoolyEditorPageState extends State<LineOutDoolyEditorPage> {
                                   Image.memory(imgData),
                                   MaterialButton(
                                     onPressed: () async {
-                                      bool result = await saveFile(imgData,"becareful_dooly");
+                                      bool result = await saveFile(imgData,"line_dooly");
                                       if (result) Navigator.of(context).pop();
                                     },
                                     child: Row(
@@ -268,7 +268,7 @@ class _LineOutDoolyEditorPageState extends State<LineOutDoolyEditorPage> {
                                   ),
                                   OutlinedButton(
                                     onPressed: () async {
-                                      shareImageFile(imgData, "becareful_dooly");
+                                      shareImageFile(imgData, "line_dooly");
                                     },
                                     child: Row(
                                       mainAxisAlignment: MainAxisAlignment.center,
@@ -355,40 +355,4 @@ class _LineOutDoolyEditorPageState extends State<LineOutDoolyEditorPage> {
     return true;
   }
 
-  void _capture() async {
-    print("START CAPTURE");
-    String datetime = DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.now());
-    var renderObject = globalKey.currentContext.findRenderObject();
-    if (renderObject is RenderRepaintBoundary) {
-      var boundary = renderObject;
-      ui.Image image = await boundary.toImage();
-      final directory = (await getApplicationDocumentsDirectory()).path;
-      print(directory);
-      ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-      Uint8List pngBytes = byteData.buffer.asUint8List();
-      print(pngBytes);
-      File imgFile = new File('$directory/want_dooly_${datetime}.png');
-      try {
-        imgFile.writeAsBytes(pngBytes);
-        print("FINISH CAPTURE ${imgFile.path}");
-      } catch (e) {
-        print(e);
-      }
-
-      try {
-        final result = await ImageGallerySaver.saveImage(pngBytes, quality: 100, name: "want_dooly_$datetime");
-        if(result['isSuccess'] == true){
-          Fluttertoast.showToast(msg: "저장성공");
-        }else{
-          Fluttertoast.showToast(msg: "저장실패");
-        }
-        
-        print(result);
-      } catch (e) {
-        print(e);
-      }
-    } else {
-      print("!");
-    }
-  }
 }
