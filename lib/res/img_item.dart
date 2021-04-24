d8aa57bb236b93ec1d1d1b896f2e3e38614ddfd8
Dreamwalker <aristojeff@gmail.com>
2021-04-24 19:19:57 +0900

    Create img_item.dart

---
+++ lib/res/img_item.dart
@@ -0,0 +1,11 @@
+
+final List<String> listImages = [
+  'assets/img/welcome.png',
+  'assets/img/damage.png',
+  'assets/img/want_bob.png',
+  'assets/img/becare.png',
+  'assets/img/line.png',
+  'assets/img/lol.png',
+  'assets/img/beam.png',
+  'assets/img/time_cosmos.png',
+];
