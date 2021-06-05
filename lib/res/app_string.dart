095d2efe0742a6ba4157b04a74ff08c075dc77f7
Dreamwalker <aristojeff@gmail.com>
2021-06-05 19:51:44 +0900

    Update app_string.dart

--- lib/res/app_string.dart
+++ lib/res/app_string.dart
@@ -18,8 +18,17 @@ const String IMG_EFFECT = "assets/img/effect.png";
 const String IMG_ICE_STAR = "assets/img/dooly_ice_star.png";
 const String IMG_SORDER_GOGILDONG = "assets/img/sord_gogildong.png";
 
+const String IMG_DOOLY_NO_ROOM = "assets/img/dooly_no_room.png";
+const String IMG_DOUNUT_00 = "assets/img/dounut_00.png";
+const String IMG_DOUNUT_01 = "assets/img/dounut_01.png";
+const String IMG_GOGILDONG_ANGRY = "assets/img/gildong_angry.png";
+const String IMG_GOGILDONG_COUGH = "assets/img/gildong_cough.png";
+const String IMG_GOGILDONG_WANT_TO_GO_HOME = "assets/img/gildong_dooly_want_go_home.png";
+const String IMG_GOGILDONG_LHLH = "assets/img/gildong_lhlh.png";
+const String IMG_GOGILDONG_DOOLY_00 = "assets/img/gogildong_dooly_00.png";
+
+
+
 const String IMG_APP_ICON = "assets/icon/icon_res_512.png";
 
 const String APP_TITLE = "둘리 짤 만들기";
-
-
